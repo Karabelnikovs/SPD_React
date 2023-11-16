04ccea2564e96c543648ef31f3ec8c1aba0eb8ec
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2023-11-16 11:04:08 +0200

    Update QuoteList.js

--- src/QuoteList.js
+++ src/QuoteList.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Quote from "./Quote";
-function QuoteList(props) {
+function QuoteList() {
   const [quote, setQuote] = useState([]);
   const [loading, setLoading] = useState(true);
 
